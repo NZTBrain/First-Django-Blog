26ea2c3c2425196cbc17c80754f0c0ad267ac5f4
Rayleigh <miraidigital.ltd@gmail.com>
2024-02-24 18:33:31 +0200

    My Django App, first commit

---
+++ python_intro.py
@@ -0,0 +1,11 @@
+'''girls = ['Monica', 'Erica', 'Rita', 'Tina', 'Sandra', 'Mary', 'Jessica', 'you']
+
+def mambo(name):
+    print('finish the lines!')
+    for name in girls:
+        print('A little bit of ' + name + '...')
+
+mambo(girls)'''
+
+for i in range(1, 6):
+    print(i)
